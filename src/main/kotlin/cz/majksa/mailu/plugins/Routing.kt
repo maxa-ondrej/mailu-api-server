7f5942154f957f994f7ee2e3994ab6f6651357de
majksa <info@majksa.cz>
2022-08-15 01:14:57 +0200

    🐛 Health checks.

--- src/main/kotlin/cz/majksa/mailu/plugins/Routing.kt
+++ src/main/kotlin/cz/majksa/mailu/plugins/Routing.kt
@@ -1,5 +1,6 @@
 package cz.majksa.mailu.plugins
 
+import cz.majksa.mailu.api.getS
 import cz.majksa.mailu.routing.domain
 import cz.majksa.mailu.routing.login
 import io.ktor.server.application.*
@@ -9,6 +10,16 @@ import io.ktor.server.routing.*
 
 fun Application.configureRouting(config: ApplicationConfig) {
     routing {
+        route("/") {
+            getS {
+                "Welcome to Mailu API"
+            }
+        }
+        route("/ping") {
+            getS {
+                "pong"
+            }
+        }
         login(config.config("ktor.auth"))
         authenticate("auth-jwt") {
             domain()
